517857dbeaff7b697b028bab381a6cce80d1e33e
Jeffrey <jz479@cornell.edu>
2015-01-20 23:42:20 -0700

    type checks

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 __author__ = 'Jeffrey'
+import sys, getopt
 
 class Evidence:
     def __init__(self, name, b_given_a, b_given_a_not):
         self.name = name
-        self.b_given_a = b_given_a
-        self.b_given_a_not = b_given_a_not
+        self.b_given_a = float(b_given_a)
+        self.b_given_a_not = float(b_given_a_not)
+
 
 class Event:
     def __init__(self, name, prior, evidence):
         self.name = name
-        self.prior = prior
+        self.prior = float(prior)
         self.evidence = evidence
+
 
     def post(self):
         p_given_a = self.prior
@@ -26,16 +29,25 @@ class Event:
 #for now it just reads from a txt document
 #format is Event:probability Evidence1:p_given_a:p_given_a_not Evidence2....
 #return: list of event objects
-
 def load_data_helper(line):
-    line.split()
-    name, prior = line[0].split(":")
-    evidence = [item.split(":") for item in line[1:]]
-    return [Event(name, a, a_not) for name, a, a_not in evidence]
+    exploded = line.split()
+    name, prior = exploded[0].split(":")
+    evidence = [item.split(":") for item in exploded[1:]]
+    evidence = [Evidence(name, a, a_not) for name, a, a_not in evidence]
+    return Event(name, prior, evidence)
+
 
 def load_data(path):
     reader = open(path)
     return [load_data_helper(line) for line in reader]
+
+
+if __name__ == "__main__":
+    opts, args = getopt.getopt(sys.argv[1:], "i:")
+    path = args[0]
+    events = load_data(path)
+    for event in events:
+        print("Event:%s Probability:%d\n" % (event.name, event.post()))
 
 
 
